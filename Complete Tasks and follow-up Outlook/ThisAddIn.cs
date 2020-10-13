f51e5728ae570b404b0a97fbf846494790969326
Alulcard <we77ard13@gmail.com>
2020-10-13 13:46:07 +0100

    Automatic Files
    Automatic files generated by Visual Studio 2019 for use on an Outlook Add-in.

---
+++ Complete Tasks and follow-up Outlook/ThisAddIn.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using Outlook = Microsoft.Office.Interop.Outlook;
+using Office = Microsoft.Office.Core;
+
+namespace Complete_Tasks_and_follow_up_Outlook
+{
+    public partial class ThisAddIn
+    {
+        // Add code here to run when the add-in is loaded in 
+        private void ThisAddIn_Startup(object sender, System.EventArgs e)
+        {
+            inspectors = this.Application.Inspectors;
+            inspectors.NewInspector +=
+            new Microsoft.Office.Interop.Outlook.InspectorsEvents_NewInspectorEventHandler(Inspectors_NewInspector);
+        }
+
+        Outlook.Inspectors inspectors;
+        void Inspectors_NewInspector(Microsoft.Office.Interop.Outlook.Inspector Inspector)
+        {
+            Outlook.MailItem mailItem = Inspector.CurrentItem as Outlook.MailItem;
+            if (mailItem != null)
+            {
+                if (mailItem.EntryID == null)
+                {
+                    mailItem.Subject = "This text was added by using code";
+                    mailItem.Body = "This text was added by using code";
+                }
+
+            }
+        }
+
+        // Add code here to run when the add-in is unloaded
+        private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
+        {
+            // Note: Outlook no longer raises this event. If you have code that 
+            //    must run when Outlook shuts down, see https://go.microsoft.com/fwlink/?LinkId=506785
+        }
+
+        #region VSTO generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisAddIn_Startup);
+            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+        }
+        
+        #endregion
+    }
+}
+
+// https://docs.microsoft.com/en-us/visualstudio/vsto/walkthrough-creating-your-first-vsto-add-in-for-outlook?view=vs-2019
+// https://docs.microsoft.com/en-us/visualstudio/vsto/programming-vsto-add-ins?view=vs-2019
+// https://docs.microsoft.com/en-us/visualstudio/vsto/outlook-object-model-overview?view=vs-2019
