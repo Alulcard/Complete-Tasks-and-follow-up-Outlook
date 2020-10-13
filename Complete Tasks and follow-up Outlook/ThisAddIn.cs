7037b9987edf008bcead902cad1aeba6246b136a
Alulcard <we77ard13@gmail.com>
2020-10-13 15:02:20 +0100

    Updated documentation

--- Complete Tasks and follow-up Outlook/ThisAddIn.cs
+++ Complete Tasks and follow-up Outlook/ThisAddIn.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Xml.Linq;
 using Outlook = Microsoft.Office.Interop.Outlook;
 using Office = Microsoft.Office.Core;
+using System.Net.Mail;
 
 namespace Complete_Tasks_and_follow_up_Outlook
 {
@@ -13,24 +14,13 @@ namespace Complete_Tasks_and_follow_up_Outlook
         // Add code here to run when the add-in is loaded in 
         private void ThisAddIn_Startup(object sender, System.EventArgs e)
         {
-            inspectors = this.Application.Inspectors;
-            inspectors.NewInspector +=
-            new Microsoft.Office.Interop.Outlook.InspectorsEvents_NewInspectorEventHandler(Inspectors_NewInspector);
+            
         }
-
-        Outlook.Inspectors inspectors;
-        void Inspectors_NewInspector(Microsoft.Office.Interop.Outlook.Inspector Inspector)
+        private void CheckTaskAsComplete()
         {
-            Outlook.MailItem mailItem = Inspector.CurrentItem as Outlook.MailItem;
-            if (mailItem != null)
-            {
-                if (mailItem.EntryID == null)
-                {
-                    mailItem.Subject = "This text was added by using code";
-                    mailItem.Body = "This text was added by using code";
-                }
+            
 
-            }
+
         }
 
         // Add code here to run when the add-in is unloaded
@@ -59,3 +49,6 @@ namespace Complete_Tasks_and_follow_up_Outlook
 // https://docs.microsoft.com/en-us/visualstudio/vsto/walkthrough-creating-your-first-vsto-add-in-for-outlook?view=vs-2019
 // https://docs.microsoft.com/en-us/visualstudio/vsto/programming-vsto-add-ins?view=vs-2019
 // https://docs.microsoft.com/en-us/visualstudio/vsto/outlook-object-model-overview?view=vs-2019
+// https://docs.microsoft.com/en-us/connectors/outlooktasks/
+// https://docs.microsoft.com/en-gb/office/client-developer/outlook/pia/tasks
+// https://www.add-in-express.com/creating-addins-blog/2013/06/12/outlook-tasks-create-get-delete/
